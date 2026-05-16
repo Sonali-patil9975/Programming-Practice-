3d7a19569b07379f6cd03afb5230744fdf9ea67c
sonali-patil9975 <sonalidhokare18@gmail.com>
2026-05-16 17:32:48 +0530

    student struct

---
+++ C Programms/StudentStruct.c
@@ -0,0 +1,48 @@
+
+#include <stdio.h>
+typedef struct {
+    int RollNo;
+    int Mark;
+    long int Pin;
+    long int PhoneNo;
+    char Name[10];
+    char perentsName[30];
+    char schoolName[10];
+    char Address[40];
+} Student;
+
+int main() {
+    Student s1;
+    Student *s = &s1;
+    printf("please enter student details:\n");
+    printf("RollNo:");
+    scanf("%d", &s->RollNo);
+    printf("Name:");
+    scanf("%s", &s->Name);
+  
+  printf("Mark:");
+    scanf("%d",&s->Mark);
+
+    printf("PerentsName:");
+    scanf("%s", &s->perentsName);
+    printf("Address:");
+    scanf("%s", &s->Address);
+   printf("schoolName:");
+     scanf("%s", &s->schoolName);
+    printf("PhoneNo:");
+    scanf("%ld", &s->PhoneNo);
+    printf("Pin:");
+    scanf("%ld", &s->Pin);
+    
+    printf("student details are:\n");
+   printf("Name = %s\n", s->Name);
+    printf("RollNo = %d\n",s->RollNo);
+    printf("Mark = %d\n",s->Mark);
+   printf("perentsName = %s\n",s->perentsName);
+   printf("school = %s\n",s->schoolName);
+   printf("Address = %s\n",s->Address);
+    printf("Pin = %ld\n",s->Pin);
+    printf("PhoneNo = %ld\n",s->PhoneNo);
+    
+    return 0;
+}

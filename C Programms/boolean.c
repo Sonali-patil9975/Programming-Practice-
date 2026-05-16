66b011c16bf12c9128ea10dd9f0bec9f385d65e7
sonali-patil9975 <sonalidhokare18@gmail.com>
2026-05-16 12:29:17 +0530

    boolean

---
+++ C Programms/boolean.c
@@ -0,0 +1,17 @@
+/* Author: Sonali Patil
+    Date: 14 may 2026
+    Description: Boolean data type in C
+*/
+    // Online C compiler to run C program online
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    bool value = false;
+    if (value == 0)
+    printf("False");
+    else if (value == 1)
+    printf("True");
+
+    return 0;
+}

9ce571c1250f9f9d162323c760b80487aeff9072
sonali-patil9975 <sonalidhokare18@gmail.com>
2026-06-09 18:51:10 +0530

    calculate power of number using funtion

---
+++ C Programms/power_funtion.c
@@ -0,0 +1,27 @@
+/* Author: Sonali Patil
+   Date: 06 june 2026
+   Discription: This program calculates the power of a number using function.
+*/
+
+// Thid program is to find the power of a number using function in C language.
+#include <stdio.h>
+// Funtion Defination
+int power (int,int);
+int main(){
+    int result;
+    int num,pow;
+    printf("Enter Number and Power:");
+    scanf("%d %d",&num,&pow);
+    result=power(num ,pow);
+    printf("result is %d",result);
+    return 0;
+}
+//Funtion Declartion
+int power(int num, int pow)
+{
+    int i, result=1;
+    for(i=1;i<=pow;i++){
+        result=result*num;
+    }
+    return result;
+}

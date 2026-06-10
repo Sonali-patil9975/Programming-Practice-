f438ee77e444f99cac414326e893ca5859937dd9
sonali-patil9975 <sonalidhokare18@gmail.com>
2026-06-10 15:59:59 +0530

    print table using funtion

---
+++ C Programms/Table.c
@@ -0,0 +1,26 @@
+/* Author: Sonali Patil
+   Date: 10 june 2026
+   Description: This program is to print the multiplication table of a given number.
+   This program is to print the multiplication table of a given number.
+
+*/
+#include <stdio.h>
+// Function Declaration
+void print_table(int);
+int main()
+{
+    int num;
+    printf(" Enter a number:");
+    scanf("%d",&num);
+    print_table(num); // Function call
+}
+// Funtion defination
+void print_table(int num)
+{
+    int i =1;
+    for (i = 1; i <= 10; i++)
+    {
+        printf("%d * %d = %d", num, i, num*i);
+        printf("\n");
+    }
+}

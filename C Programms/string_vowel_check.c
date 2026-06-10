bf39fc7227c95ee70a6da7b0035001ea491040ce
sonali-patil9975 <sonalidhokare18@gmail.com>
2026-06-10 13:11:34 +0530

    check string contains vowels or not using function

---
+++ C Programms/string_vowel_check.c
@@ -0,0 +1,57 @@
+/* Author: Sonali Patil
+   Date: 09 june 2026
+   Description: This program checks if a string contains vowels or not using function.
+*/
+
+// This program checks if a string contains vowels or not
+#include <stdio.h>
+int check_vowels(char[]);
+int string_length(char[]);
+int main() 
+{
+    int yes;
+    char str[50];
+    printf("Enter string:");
+    scanf("%s",str);
+    yes = check_vowels(str);
+    if (yes == 1)
+    printf("%s cantain vowels",str);
+    else
+    printf("%s does not cantain vowels",str);
+
+    return 0;
+}
+int string_length(char array[])
+{
+    int i = 0;
+    if (array == NULL)
+    return 0;
+    while (array[i] != '\0')
+    {
+        i++;
+    }
+    return i;
+}
+// Funtion declaration
+int check_vowels(char str[]){
+    int n, i;
+    char vowels[] = {'a','e','i','o','u'};
+    n = string_length(str);
+    for(i = 0; i < n; i++)
+    {
+        if 
+        (str[i] == vowels[0]||
+        str[i] == vowels[1]||
+        str[i] == vowels[2]||
+        str[i] == vowels[3]||
+        str[i] == vowels[4])
+        {
+            return 1;
+        }
+    }    
+    
+    return 0;
+    
+}
+        
+    

419f7a9a4c6fbfccb91c0a2df0e9f42621a17a98
sonali-patil9975 <sonalidhokare18@gmail.com>
2026-06-18 15:13:29 +0530

    matrix addition

---
+++ C Programms/matrix addition.c
@@ -0,0 +1,51 @@
+/* Author: Sonali Patil
+   Date: 17 June 2026
+   Description: This program is used to add two matrices of size 2x2.
+*/
+// This program is used to add two matrices of size 2x2
+#include<stdio.h>
+int main()
+{
+    int i,j,row =2, column =2;
+    int matrix1[3][3],matrix2[3][3];
+    //Read matrix1
+    printf("Please enter Matrix 1\n");
+    for (i = 0; i < row; i++)
+    {
+        for(j = 0; j < row; j++)
+        {
+            printf("Enter %d row & %d column element:",i,j);
+            scanf("%d",&matrix1[i][j]);
+        }
+    }
+    // Read matrix2  
+    printf("\nPlease enter matrix 2\n");
+    for(i = 0; i < row; i++)
+    {
+        for(j = 0; j < column; j++)
+        {
+        printf("Enter %d row & %d column element:",i,j);
+        scanf("%d", &matrix2[i][j]);
+        }
+    }
+    int matrix3[3][3];
+    for(i = 0; i < row; i++)
+    {
+        for(j = 0; j < column; j++)
+        {
+            matrix3[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+    // print
+    printf("Addition of Matrix 1 and Matrix 2 = \n");
+    for(i = 0; i < row; i++)
+    {
+        for(j = 0; j < column; j++)
+        {
+            printf("%d \t",matrix3[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+
+}

5b6469528bb73e2f2e93e842e2981ce7ce3aedea
sonali-patil9975 <sonalidhokare18@gmail.com>
2026-07-24 12:34:51 +0530

    check num is palindrom or not

---
+++ Python Programs/palindrom.py
@@ -0,0 +1,17 @@
+def main():
+    num = int(input("Enter a number: "))
+    original = num
+    reversed_num = 0
+
+    while num > 0:
+        digit = num % 10
+        reversed_num = reversed_num * 10 + digit
+        num //= 10
+
+    if original == reversed_num:
+        print(f"{original} is a palindrome.")
+    else:
+        print(f"{original} is not a palindrome.")
+
+if __name__ == "__main__":
+    main()

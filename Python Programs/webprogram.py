fe958917f06190413c1eb86d565e6d5699ae254e
sonali-patil9975 <sonalidhokare18@gmail.com>
2026-07-28 17:00:09 +0530

    sample web program

---
+++ Python Programs/webprogram.py
@@ -0,0 +1,13 @@
+import requests 
+
+
+url = "https://randomuser.me/api/"
+response = requests.get(url)
+
+print("status code =", response.status_code)
+resp = response.json()
+print("response =", resp)
+print("status message =", resp.get("results")[0].get("name").get("first"))
+print("Gender =", resp.get("results")[0].get("gender"))
+print("Phone  =", resp.get("results")[0].get("cell"))
+

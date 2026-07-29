1bbeec57e6a64cd12ca091595a93b75b9973b3f4
sonali-patil9975 <sonalidhokare18@gmail.com>
2026-07-29 12:36:22 +0530

    sample web program

--- Python Programs/webprogram.py
+++ Python Programs/webprogram.py
@@ -10,4 +10,5 @@ print("response =", resp)
 print("status message =", resp.get("results")[0].get("name").get("first"))
 print("Gender =", resp.get("results")[0].get("gender"))
 print("Phone  =", resp.get("results")[0].get("cell"))
+print("country =", resp.get("results")[0].get("location").get("country"))
 
